5f9630e891fad83b254119d00bb1324b03149acf
Timo Ley <timo.ley@tilera.dev>
2023-02-28 19:18:59 +0100

    chore: update capes code

--- src/main/java/dev/tilera/capes/Capes.java
+++ src/main/java/dev/tilera/capes/Capes.java
@@ -1,6 +1,11 @@
 package dev.tilera.capes;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import com.jadarstudios.developercapes.DevCapes;
+
+import org.apache.commons.io.IOUtils;
 
 import net.minecraftforge.common.MinecraftForge;
 
@@ -12,8 +17,12 @@ public class Capes {
         if (!capesInitialized) {
             capesInitialized = true;
             try {
+                URL index = new URL("https://capes.tilera.xyz/devcapes/index");
+                InputStream stream = DevCapes.getInstance().getStreamForURL(index);
+                String capeConfig = IOUtils.toString(stream);
+                stream.close();
                 DevCapes.getInstance().registerConfig(
-                    "https://capes.tilera.xyz/devcapes.json"
+                    capeConfig
                 );
                 MinecraftForge.EVENT_BUS.register(new RenderEventHandler());
             } catch (Exception e) {
